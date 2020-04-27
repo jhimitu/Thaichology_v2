7775f2771d0b90c92c1a96a0f9ed37169788e1d2
Jhia Turner <jhimitu@gmail.com>
2020-04-26 17:30:31 -0700

    implement mobile-view overlay menu

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 'use strict';
+
+function openNavigationMenu() {
+  document.getElementById('nav').style.width = '100%';
+}
+
+function closeNavigationMenu() {
+  document.getElementById('nav').style.width = '0%';
+}
